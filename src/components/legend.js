947511643d97ed89b987d3e9098f8081b5b9bf29
Matthew Pullen <matthew.pullen@gmail.com>
2022-10-28 20:54:58 -0500

    Added commas to legend numbers

--- src/components/legend.js
+++ src/components/legend.js
@@ -4,10 +4,10 @@ import './legend.css';
 const Legend = () => {
     return (
         <div className="legend">
-            <div style={{ "--color": '#fe0131' }}>Over 10000</div>
-            <div style={{ "--color": '#e90069' }}>5000 - 9999</div>
-            <div style={{ "--color": '#bd2d8f' }}>2500 - 4999</div>
-            <div style={{ "--color": '#84489d' }}>1000 - 2499</div>
+            <div style={{ "--color": '#fe0131' }}>Over 10,000</div>
+            <div style={{ "--color": '#e90069' }}>5,000 - 9,999</div>
+            <div style={{ "--color": '#bd2d8f' }}>2,500 - 4,999</div>
+            <div style={{ "--color": '#84489d' }}>1,000 - 2,499</div>
             <div style={{ "--color": '#4b5294'}}>100 - 999</div>
             <div style={{ "--color": '#265079' }}>1 - 99</div>
             <div style={{ "--color": '#fee5d9' }}>No data</div>
